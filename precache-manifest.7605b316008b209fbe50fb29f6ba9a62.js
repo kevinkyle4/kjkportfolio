cd330d3bd1736d00c7358922228fb8431c4267f8
Kevin Kyle <kevinkyle4@gmail.com>
2021-01-21 15:27:20 -0500

    Updates

---
+++ precache-manifest.7605b316008b209fbe50fb29f6ba9a62.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e4289d8e178ce1774f0b86c8f6c14d",
+    "url": "/kjkportfolio/index.html"
+  },
+  {
+    "revision": "35f6ca98fb5d77536ca8",
+    "url": "/kjkportfolio/static/css/2.22fd3c20.chunk.css"
+  },
+  {
+    "revision": "b11e835ca88459a65f6b",
+    "url": "/kjkportfolio/static/css/main.398ebf96.chunk.css"
+  },
+  {
+    "revision": "35f6ca98fb5d77536ca8",
+    "url": "/kjkportfolio/static/js/2.0868d788.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kjkportfolio/static/js/2.0868d788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11e835ca88459a65f6b",
+    "url": "/kjkportfolio/static/js/main.16fd8b81.chunk.js"
+  },
+  {
+    "revision": "55da45d456033572ce9d",
+    "url": "/kjkportfolio/static/js/runtime-main.442720c2.js"
+  },
+  {
+    "revision": "0b6b2fe648b2b7fd2814cc338dea36d0",
+    "url": "/kjkportfolio/static/media/app2.0b6b2fe6.png"
+  },
+  {
+    "revision": "932307766664f74e0f02853128b8f659",
+    "url": "/kjkportfolio/static/media/cover-new.93230776.jpg"
+  },
+  {
+    "revision": "ad3c3f36ccfbe6e6e20dbb004672811d",
+    "url": "/kjkportfolio/static/media/cover-new3.ad3c3f36.jpg"
+  },
+  {
+    "revision": "29f507a75847c01b7bf404c568a1e137",
+    "url": "/kjkportfolio/static/media/desktopscreenshop2.29f507a7.png"
+  },
+  {
+    "revision": "51c072e0e9667e7addb3df65065a7c75",
+    "url": "/kjkportfolio/static/media/eapi-mobile.51c072e0.png"
+  },
+  {
+    "revision": "e014034ec08c08eecbb1ba0f9b6ebfbe",
+    "url": "/kjkportfolio/static/media/engapiprojects.e014034e.png"
+  },
+  {
+    "revision": "f672db6fcd15ebd257b0703db18cb81e",
+    "url": "/kjkportfolio/static/media/engineerapinew1.f672db6f.png"
+  },
+  {
+    "revision": "b6775ca5266a3dcc53c628ef6a94c3da",
+    "url": "/kjkportfolio/static/media/engineerapinew2.b6775ca5.png"
+  },
+  {
+    "revision": "d01171bb583c00d8c51dbfe10e6cf37c",
+    "url": "/kjkportfolio/static/media/mgmtgif.d01171bb.gif"
+  },
+  {
+    "revision": "125ca73b3226253beb397016798cafa6",
+    "url": "/kjkportfolio/static/media/pphoto.125ca73b.jpg"
+  },
+  {
+    "revision": "042c0002d19394100503bdcfca066db3",
+    "url": "/kjkportfolio/static/media/profphoto1.042c0002.png"
+  },
+  {
+    "revision": "e9c4a7ec69fd49eb58a926e1a11daed1",
+    "url": "/kjkportfolio/static/media/rcrmdesktop.e9c4a7ec.png"
+  },
+  {
+    "revision": "dcd829c081a74a6e5c616f6872876caf",
+    "url": "/kjkportfolio/static/media/stonks-main.dcd829c0.png"
+  }
+]);
